3f4d4b0b8d01e5204764387d8524e09872148620
Christian Oliveira <chrisfrlps1@gmail.com>
2025-05-22 17:35:36 -0300

    feat: controller, service, repository, DTO

--- src/main/java/com/devsuperior/dslist/DslistApplication.java
+++ src/main/java/com/devsuperior/dslist/DslistApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("entities")
+@EntityScan("com/devsuperior/dslist/entities")
 public class DslistApplication {
 
 	public static void main(String[] args) {
